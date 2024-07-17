0121d37a417e1c8cdea273bf951e0486d5348adf
Jon Reades <jon@reades.com>
2024-07-17 12:23:20 +0100

    Q&A working

--- _extensions/quarto-ext/fontawesome/fontawesome.lua
+++ _extensions/quarto-ext/fontawesome/fontawesome.lua
@@ -14,38 +14,6 @@ local function isEmpty(s)
   return s == nil or s == ''
 end
 
-local function getRgbTriplets(s)
-  local triplet = {}
-  for t in string.gmatch(string.match(s, 'rgb%(([^%)]+)%)'), "([^,]+)") do
-    table.insert(triplet, t)
-  end
-  return triplet
-end
-
-local function toRgbCss(s)
-  local triplet = {}
-  for k,t in pairs(getRgbTriplets(s)) do
-    if tonumber(t) <= 1 then
-      table.insert(triplet, string.format("%.0f", tonumber(t) * 255))
-    else
-      table.insert(triplet, t)
-    end
-  end
-  return table.concat(triplet, ",")
-end
-
-local function toRgbTex(s)
-  local triplet = {}
-  for k,t in pairs(getRgbTriplets(s)) do
-    if tonumber(t) > 1 then
-      table.insert(triplet, string.format("%.2f", tonumber(t) / 255))
-    else
-      table.insert(triplet, t)
-    end
-  end
-  return table.concat(triplet, ",")
-end
-
 local function isValidSize(size)
   local validSizes = {
     "tiny",
@@ -90,7 +58,6 @@ return {
     end
 
     local size = pandoc.utils.stringify(kwargs["size"])
-    local color = pandoc.utils.stringify(kwargs["color"])
     
     -- detect html (excluding epub which won't handle fa)
     if quarto.doc.is_format("html:js") then
@@ -98,34 +65,18 @@ return {
       if not isEmpty(size) then
         size = " fa-" .. size
       end
-      if not isEmpty(color) then
-        if string.find(color, '^rgb') then
-          color = " style=\"color:rgb(" .. toRgbCss(color) .. ");\""
-        else
-          color = " style=\"color:" .. color .. ";\""
-        end
-      end
       return pandoc.RawInline(
         'html',
-        "<i class=\"fa-" .. group .. " fa-" .. icon .. size .. "\"" .. title .. label .. color .. "></i>"
+        "<i class=\"fa-" .. group .. " fa-" .. icon .. size .. "\"" .. title .. label .. "></i>"
       )
     -- detect pdf / beamer / latex / etc
     elseif quarto.doc.is_format("pdf") then
       ensureLatexDeps()
-      local tex = "\\faIcon{" .. icon .. "}"
-      if not isEmpty(isValidSize(size)) then
-        tex = "{\\" .. size .. tex .. "}"
+      if isEmpty(isValidSize(size)) then
+        return pandoc.RawInline('tex', "\\faIcon{" .. icon .. "}")
+      else
+        return pandoc.RawInline('tex', "{\\" .. size .. "\\faIcon{" .. icon .. "}}")
       end
-      if not isEmpty(color) then
-        if string.find(color, '^#%x+') then 
-          tex = "\\textcolor[HTML]{" .. string.match(color, '#(%x+)') .. "}{" .. tex .. "}"
-        elseif string.find(color, '^rgb') then
-          tex = "\\textcolor[rgb]{" .. toRgbTex(color) .. "}{" .. tex .. "}"
-        else
-          tex = "\\textcolor{" .. color .. "}{" .. tex .. "}"
-        end
-      end
-      return pandoc.RawInline('tex', tex)
     else
       return pandoc.Null()
     end
