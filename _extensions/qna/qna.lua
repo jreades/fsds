68bd0bdd4c5ee15d59801728db648ccab8d2f9e2
Jon Reades <jon@reades.com>
2024-07-18 21:33:03 +0100

    Finally have code blocks working in the Q&A panel setup.

--- _extensions/qna/qna.lua
+++ _extensions/qna/qna.lua
@@ -125,11 +125,15 @@ function Div(div)
 
     cb = pandoc.CodeBlock(raw[1].content[1].text, pandoc.Attr('', {'python','cell-code'}, {}))
     cd = pandoc.Div(cb, pandoc.Attr('', {'cell'}, {}))
-    if (raw[1].content[2] ~= nil and raw[1].content[2].t == 'CodeBlock') then
-      co = pandoc.Div(raw[1].content[2].text, pandoc.Attr('', {"cell-output", "cell-output-stdout"}, {}))
+    
+    if (raw[1].content[2] ~= nil and raw[1].content[2].t=='CodeBlock') then
+      print("Adding...")
+      co = pandoc.Div(pandoc.CodeBlock(raw[1].content[2].text), pandoc.Attr('', {"cell-output", "cell-output-stdout"}, {}))
       table.insert(cd.content,co)
     end
     hd = pandoc.Header(header_level, raw[1].title, pandoc.Attr('', {'qna-question'}, {}))
+
+    -- print(cd)
 
     resources[1] = hd
     resources[2] = cd
