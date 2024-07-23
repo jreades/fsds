9fc5ae1b709e9d3e9340695c80f9da0d7413d13e
Jon Reades <jon@reades.com>
2024-07-23 21:02:29 +0100

    Got multiple code blocks working.

--- _extensions/qna/qna.lua
+++ _extensions/qna/qna.lua
@@ -1,21 +1,3 @@
-local function render_quarto_tab_orig(tbl, tabset)
-  local content = quarto.utils.as_blocks(tbl.content)
-  local title = quarto.utils.as_inlines(tbl.title)
-  local inner_content = pandoc.List()
-  inner_content:insert(pandoc.Header(tabset.level, title))
-  inner_content:extend(content)
-  return pandoc.Div(inner_content)
-end
-
-local function render_quarto_tab(tbl, hlevel)
-  local content = quarto.utils.as_blocks(tbl.content)
-  local title = quarto.utils.as_inlines(tbl.title)
-  local inner_content = pandoc.List()
-  inner_content:insert(pandoc.Header(hlevel, title))
-  inner_content:extend(content)
-  return pandoc.Div(inner_content)
-end
-
 function Div(div)
   if not div.classes:includes("qna") then 
     return
@@ -25,11 +7,11 @@ function Div(div)
   local div_num = 0
   local header_level = 0
 
-  -- print("~~~~~ Q&A ~~~~~")
+  -- print("~~~~~ Q&A div found ~~~~~")
   -- for key, value in pairs(div) do
   --   print(key, value)
   -- end
-  -- print("~~~~~~~~~~~~~~")
+  -- print("~~~~~~~~~~~~~~~~~~~~~~~~~")
 
   div:walk({
       Block = function(block)
@@ -104,10 +86,13 @@ function Div(div)
   -- the second. We remove the question *or* answer
   -- for some output formats based on filter spec
   if quarto.doc.is_format("ipynb") then
+    -- print("ipynb")
     table.remove(raw,2)
   elseif quarto.doc.is_format("pdf") then
+    -- print("pdf")
     table.remove(raw,1)
   elseif quarto.doc.is_format("html") then 
+    -- print("html")
     -- do nothing
   else
     print("No output format detected.")
@@ -115,7 +100,6 @@ function Div(div)
   
   if #raw == 1 then
     local resources = {}
-    local content   = {}
     
     -- print("~~~~~")
     -- for key, value in pairs(raw[1].content) do
@@ -123,21 +107,56 @@ function Div(div)
     -- end
     -- print("~~~~~")
 
-    cb = raw[1].content[1] -- pandoc.CodeBlock(raw[1].content[1].text, pandoc.Attr('', {'python','cell-code'}, {}))
-    cd = pandoc.Div(cb, pandoc.Attr('', {'cell'}, {}))
-    
-    if (raw[1].content[2] ~= nil and raw[1].content[2].t=='CodeBlock') then
-      if raw[1].content[2].text ~= nil then
-        co = pandoc.Div(pandoc.CodeBlock(raw[1].content[2].text), pandoc.Attr('', {"cell-output", "cell-output-stdout"}, {}))
-        table.insert(cd.content,co)
+    hd = pandoc.Header(header_level, raw[1].title, pandoc.Attr('', {'qna-question'}, {}))
+    resources[1] = hd
+
+    local nxt = nil
+
+    for key, value in pairs(raw[1].content) do 
+      -- print("~~~~~~~~~~~")
+      -- print("Key: ", key)
+      -- print("Value: ", value)
+      -- print("Type: ", value.t)
+
+      if value.t == 'CodeBlock' then 
+        -- Basically, check here to see if we're on the next
+        -- key in a code block which is most likely the code
+        -- block's *output* (we save a ref to the 'next' item).
+        -- So next should only have a value if we just *saw*
+        -- a code block and processed its output as well.
+        if nxt ~= nil then
+          nxt = nil
+        
+        else
+          -- Create a code div (cd)
+          cd = pandoc.Div(value, pandoc.Attr('', {'cell'}, {}))
+          -- print(" > cd: ", cd)
+
+          -- We seem to have to assume that if the next 'thing'
+          -- is a code block then it has to be output. I can't
+          -- figure out how to get Lua to do this properly by
+          -- checking the class of the next output to see if 
+          -- it's new code in a separate block or output of code.
+          nxt = raw[1].content[key+1]
+          -- If the next thing looks like it is likely code output
+          if (nxt ~= nil and nxt.t == 'CodeBlock' and nxt.text ~= nil) then
+
+            -- Create the code output (co)
+            co = pandoc.Div(nxt, pandoc.Attr('', {"cell-output", "cell-output-stdout"}, {}))
+            -- print(" > co: ",co)
+            
+            -- And insert it into the code div
+            table.insert(cd.content,co)
+          else
+            nxt = nil
+          end
+          -- Add it to the resources stack
+          resources[#resources+1] = cd
+        end
+      else
+        resources[#resources+1] = value
       end
     end
-    hd = pandoc.Header(header_level, raw[1].title, pandoc.Attr('', {'qna-question'}, {}))
-
-    -- print(cd)
-
-    resources[1] = hd
-    resources[2] = cd
 
     return resources
   else
