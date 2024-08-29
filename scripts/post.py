b8fd85707033a85963b3d0df443bb19a3d7ab7f2
Jon Reades <jon@reades.com>
2024-08-29 07:50:04 +0100

    Reorganising the YAML files to support Quarto profiles: https://quarto.org/docs/projects/profiles.html since this seems a better way to manage outputting the practicals in a very different way from the rest of the site.

---
+++ scripts/post.py
@@ -0,0 +1,10 @@
+print("~" * 50)
+msg = """
+Don't forget to render the practicals!
+> quarto render practicals --to ipynb
+> quarto render ipynb practicals --to pdf
+> git add practicals && git commit -m "Updated practicals" && git push
+Otherwise these won't be updated.
+"""
+print(msg)
+print("~" * 50)
