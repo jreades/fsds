cfc7155d4db838dbed0d64b2ff9dd202d7f4f5bf
Jon Reades <jon@reades.com>
2024-09-23 14:35:10 +0100

    Added note on publishing.

--- scripts/post.py
+++ scripts/post.py
@@ -2,3 +2,4 @@ print("~" * 50)
 msg = """Don't forget to generate the answer PDFs.\nThese are generated to `_answers` using the `prac` profile  while the web content is in `_site`."""
 print(msg)
 print("~" * 50)
+print("To publish (if you've already rendered: `quarto publish gh-pages --no-render`")
