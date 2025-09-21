532c3d6df662a5dc13ba7fcc7046f46776c5f09e
Jon Reades <jon@reades.com>
2025-09-21 09:03:40 +0100

    Finishing up conversion to latest version.

--- scripts/post.py
+++ scripts/post.py
@@ -1,5 +1,5 @@
 print("~" * 50)
-msg = """Don't forget to also generate the answer PDFs using:\n    quarto render --profile pracs\nThese are generated to `_answers` using the `prac` profile while the web content is in `_site`."""
+msg = """Don't forget to also generate the answer PDFs using:\n    quarto render --profile pracs practicals\nThese are generated to `_answers` using the `prac` profile while the web content is in `_site`."""
 print(msg)
 print("~" * 50)
 print("To publish once you've rendered run: `quarto publish gh-pages --no-render`")
--- scripts/pracs-pre.py
+++ scripts/pracs-pre.py
@@ -1,5 +1,11 @@
+import shutil
+from pathlib import Path
+rootdir = Path("_answers").resolve()
+print(f"Removing {rootdir}")
+shutil.rmtree(rootdir)
 print("~" * 50)
 msg = """Rendering the practicals..."""
-# > quarto render --profile pracs
+# > quarto render --profile pracs practicals
 print(msg)
 print("~" * 50)
+exit()
